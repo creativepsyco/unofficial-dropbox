5a4b2e9924e2cf0dff8d11b3ad1cc1c5be977506
Mohit Kanwal <msk@nus.edu.sg>
2012-02-21 11:02:28 +0800

    Changed the IVLE File Sync Logo + Fixes to System Tray Code

--- IvleFileSync/src/ivlefilesync/IVLESystemTray.java
+++ IvleFileSync/src/ivlefilesync/IVLESystemTray.java
@@ -7,103 +7,109 @@ package ivlefilesync;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.net.*;
 import javax.swing.*;
-import java.util.*;
 
 /**
- *
+ * 
  * @author msk
  */
 public class IVLESystemTray {
 
-    public void RunTray() {
-        final TrayIcon trayIcon;
+	public void RunTray() {
+		
+		final TrayIcon trayIcon;
 
-        if (SystemTray.isSupported()) {
-            SystemTray tray = SystemTray.getSystemTray();
-//            Image image = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/splash.png"));
-            Image image = new ImageIcon(this.getClass().getResource("resources/ivle_sync_logo.png")).getImage();
-            ActionListener exitListener = new ActionListener() {
+		if (SystemTray.isSupported()) {
+			SystemTray tray = SystemTray.getSystemTray();
+			// Image image =
+			// Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/splash.png"));
+			Image image = new ImageIcon(this.getClass().getResource(
+					"resources/reload.png")).getImage();
+			
+			/* All the Action Listeners */
+			ActionListener exitListener = new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					System.out.println("Exiting...");
+					System.exit(0);
+				}
+			};
 
-                public void actionPerformed(ActionEvent e) {
-                    System.out.println("Exiting...");
-                    System.exit(0);
-                }
-            };
+			ActionListener manuallySyncListener = new ActionListener() {
+				public void actionPerformed(ActionEvent ae) {
+					
+					//TODO: Check first for being able to manually download or not
+					
+					Device_SyncResult res = IVLEClientHelper.SyncAndDownload(
+							IVLEOfflineStorage
+									.GetPropertyValue(Constants.UserID),
+							IVLEOfflineStorage
+									.GetPropertyValue(Constants.APIKey));
+					//TODO: what happens on a sync failure!Must check for value
+				}
+			};
 
+			ActionListener setPreferencesActionListener = new ActionListener() {
 
-            ActionListener manuallySyncListener = new ActionListener() {
+				public void actionPerformed(ActionEvent ae) {
+					frmPreferences myPreferences = new frmPreferences();
+					IvleFileSyncApp.getApplication().show(myPreferences);
+				}
+			};
 
-                public void actionPerformed(ActionEvent ae) {
-                    Device_SyncResult res = IVLEClientHelper.SyncAndDownload(
-                           IVLEOfflineStorage.GetPropertyValue(Constants.UserID),
-                            IVLEOfflineStorage.GetPropertyValue(Constants.APIKey));
-                    javax.swing.JOptionPane.showMessageDialog(null, res.Success + "\n" + res.LastSync.toString());
-                }
-            };
+			ActionListener aboutBoxActionListener = new ActionListener() {
+				
+				public void actionPerformed(ActionEvent ae) {
+					showAboutBox();
+				}
+				
+				private void showAboutBox() {
+					throw new UnsupportedOperationException(
+							"Not yet implemented");
+				}
+			};
 
-            ActionListener setDirItemActionListener = new ActionListener() {
+			PopupMenu popup = new PopupMenu();
 
-                public void actionPerformed(ActionEvent ae) {
-                    frmPreferences myPreferences = new frmPreferences();
-                    IvleFileSyncApp.getApplication().show(myPreferences);
-                }
-            };
+			MenuItem anotherItem = new MenuItem("Manually Sync");
+			anotherItem.addActionListener(manuallySyncListener);
+			popup.add(anotherItem);
 
-            ActionListener aboutBoxActionListener = new ActionListener() {
+			MenuItem setDirItem = new MenuItem("Settings...");
+			setDirItem.addActionListener(setPreferencesActionListener);
+			popup.add(setDirItem);
 
-                public void actionPerformed(ActionEvent ae) {
-                    showAboutBox();
-                }
+			MenuItem aboutBoxItem = new MenuItem("About");
+			aboutBoxItem.addActionListener(aboutBoxActionListener);
+			popup.add(aboutBoxItem);
 
-                private void showAboutBox() {
-                    throw new UnsupportedOperationException("Not yet implemented");
-                }
-            };
+			MenuItem defaultItem = new MenuItem("Exit");
+			defaultItem.addActionListener(exitListener);
+			popup.add(defaultItem);
 
-            PopupMenu popup = new PopupMenu();
+			trayIcon = new TrayIcon(image, "IVLE File Sync", popup);
 
-            MenuItem anotherItem = new MenuItem("Manually Sync");
-            anotherItem.addActionListener(manuallySyncListener);
-            popup.add(anotherItem);
+			ActionListener actionListener = new ActionListener() {
 
-            MenuItem setDirItem = new MenuItem("Settings...");
-            setDirItem.addActionListener(setDirItemActionListener);
-            popup.add(setDirItem);
+				public void actionPerformed(ActionEvent e) {
+					trayIcon.displayMessage("Action Event",
+							"An Action Event Has Been Performed!",
+							TrayIcon.MessageType.INFO);
+				}
+			};
 
-            MenuItem aboutBoxItem = new MenuItem("About");
-            aboutBoxItem.addActionListener(aboutBoxActionListener);
-            popup.add(aboutBoxItem);
+			trayIcon.setImageAutoSize(true);
+			trayIcon.addActionListener(actionListener);
 
-            MenuItem defaultItem = new MenuItem("Exit");
-            defaultItem.addActionListener(exitListener);
-            popup.add(defaultItem);
+			try {
+				tray.add(trayIcon);
+			} catch (AWTException e) {
+				System.err.println("TrayIcon could not be added.");
+			}
 
-
-            trayIcon = new TrayIcon(image, "IVLE FileSync", popup);
-
-            ActionListener actionListener = new ActionListener() {
-
-                public void actionPerformed(ActionEvent e) {
-                    trayIcon.displayMessage("Action Event",
-                            "An Action Event Has Been Performed!",
-                            TrayIcon.MessageType.INFO);
-                }
-            };
-
-            trayIcon.setImageAutoSize(true);
-            trayIcon.addActionListener(actionListener);
-
-            try {
-                tray.add(trayIcon);
-            } catch (AWTException e) {
-                System.err.println("TrayIcon could not be added.");
-            }
-
-        } else {
-            //  System Tray is not supported
-        }
-    }
+		} else {
+			// System Tray is not supported
+			// TODO: Display message for unsupported feature
+		}
+	}
 
 }
