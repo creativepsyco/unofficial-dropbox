00fe5a40dd268dc4584791afb12ffc37cf99ab39
Mohit Kanwal <mohit.kanwal@gmail.com>
2012-08-13 02:21:09 +0800

    Updating Project with Copyright!

--- IvleFileSync/src/ivlefilesync/Constants.java
+++ IvleFileSync/src/ivlefilesync/Constants.java
@@ -1,4 +1,20 @@
 /*
+ * Copyright (c) 2012. $author
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -7,14 +23,14 @@ package ivlefilesync;
 
 /**
  * Constant Keys used for accessing keys of properties file
- * 
+ *
  * @author msk
  */
 public class Constants {
-	public static final String DeviceID = "DeviceID";
-	public static final String FileQueue = "FileQueue";
-	public static final String SyncDirectory = "SyncDirectory";
-	public static final String UserID = "UserID";
-	public static final String APIKey = "APIKey";
-	public static final String FIRST_TIME_START = "FirstTimeStart";
+    public static final String DeviceID = "DeviceID";
+    public static final String FileQueue = "FileQueue";
+    public static final String SyncDirectory = "SyncDirectory";
+    public static final String UserID = "UserID";
+    public static final String APIKey = "APIKey";
+    public static final String FIRST_TIME_START = "FirstTimeStart";
 }
